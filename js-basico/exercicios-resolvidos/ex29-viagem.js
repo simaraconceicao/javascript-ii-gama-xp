4425e022d6700f978ab83f5c05604ad4eaf5c712
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:41:52 -0300

    exercicio29

---
+++ js-basico/exercicios-resolvidos/ex29-viagem.js
@@ -0,0 +1,8 @@
+// Exercício 29 - Viagem
+// Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+console.log("\n\n\nExercício 29 - Viagem");
+function tempoDeViagem(distancia, velocidadeMedia) {
+    return distancia / velocidadeMedia;
+}
+
+console.log(tempoDeViagem(120,60));
