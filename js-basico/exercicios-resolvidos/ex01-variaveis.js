9208ab9afec4230e43fbf6aaeef49577e41172a7
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 02:05:56 -0300

    exercicio01

---
+++ js-basico/exercicios-resolvidos/ex01-variaveis.js
@@ -0,0 +1,14 @@
+// Exercício 1 - Variaveis
+// Vamos criar variaveis usando let, const e var, atribuir valores, identificar comportamentos e saídas quando tentarmos recuperar.
+console.log("Exercício 1 - Variaveis");
+var v1 = 10;
+let l1 = 20;
+const c1 = 30;
+
+console.log(v1);
+console.log(l1);
+console.log(c1);
+
+console.log(v1 %= 4);
+console.log(l1 /= 5);
+//console.log(c1 *= 2) // Erro: const não pode ser alterada
