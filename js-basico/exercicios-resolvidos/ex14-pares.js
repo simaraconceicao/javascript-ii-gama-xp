1cb7060bd549d828de3114d1eefdfb1b220fc2cd
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:25:14 -0300

    exercicio14

---
+++ js-basico/exercicios-resolvidos/ex14-pares.js
@@ -0,0 +1,10 @@
+// Exercício 14 - Pares
+// Imprima na tela os números pares existentes entre 0 e 100..
+console.log("\n\n\nExercício 14 - Pares");
+function pares() {
+    for(let i = 0; i <= 100; i += 2) {
+        console.log(i);
+    }
+}
+
+pares();
