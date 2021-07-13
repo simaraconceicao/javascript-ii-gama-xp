806d8de470ec72a156a85a0e7acbe7600382b0db
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:37:10 -0300

    exercicio24

---
+++ js-basico/exercicios-resolvidos/ex24-dias-em-horas.js
@@ -0,0 +1,8 @@
+// Exercício 24 - Dias em horas
+// Crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+console.log("\n\n\nExercício 24 - Dias em horas");
+function daysToHours(value) {
+    return value * 24;
+}
+
+console.log(daysToHours(30));
