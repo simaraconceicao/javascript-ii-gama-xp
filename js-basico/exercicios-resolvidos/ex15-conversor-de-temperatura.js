8295dc0face24c76c834826d9157b13232364ce1
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:26:42 -0300

    exercicio15

---
+++ js-basico/exercicios-resolvidos/ex15-conversor-de-temperatura.js
@@ -0,0 +1,8 @@
+// Exercício 15 - Conversor de temperatura
+// Crie uma função que recebe uma temperatura em graus celsius retorna o valor convertido em Fahrenheit.
+console.log("\n\n\nExercício 15 - Conversor de temperatura");
+function celsiusToFahrenheit(value) {
+    return value * 1.8 + 32;
+}
+
+console.log(celsiusToFahrenheit(32));
