8f19b071f42ff28087ef6f22fb245ff4e747a595
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:30:59 -0300

    exercicio18

---
+++ js-basico/exercicios-resolvidos/ex18-porcentagem.js
@@ -0,0 +1,8 @@
+// Exercício 18 - Porcentagem
+// Crie uma função que calcula 5% de desconto retornando o valor do desconto.
+console.log("\n\n\nExercício 18 - Porcentagem");
+function cincoPorCento(value) {
+    return value * 0.05;
+}
+
+console.log(cincoPorCento(15000));
