ea3ea932de7ededf403a5a3b2affaea495184175
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:29:47 -0300

    exercicio17

---
+++ js-basico/exercicios-resolvidos/ex17-multiplica.js
@@ -0,0 +1,8 @@
+// Exercício 17 - Multiplica
+// Crie uma função que recebe 2 parâmetros e retorna a multiplicação entre else.
+console.log("\n\n\nExercício 17 - Multiplica");
+function multiplica(a,b) {
+    return a * b;
+}
+
+console.log(multiplica(5,6));
