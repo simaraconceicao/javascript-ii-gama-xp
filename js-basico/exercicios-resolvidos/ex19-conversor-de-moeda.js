d3aaddbc24e8502900c5eaa9faff7d96244c810c
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:31:56 -0300

    exercicio19

---
+++ js-basico/exercicios-resolvidos/ex19-conversor-de-moeda.js
@@ -0,0 +1,8 @@
+// Exercício 19 - Conversor de moeda
+// Crie uma função que irá converter uma quantia de real para dolar utilizando a cotação do dia.
+console.log("\n\n\nExercício 19 - Conversor de moeda");
+function realToDollar(value) {
+    return value / 5.17;
+}
+
+console.log(realToDollar(200));
