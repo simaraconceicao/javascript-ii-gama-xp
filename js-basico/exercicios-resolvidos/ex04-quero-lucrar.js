140cbfc364890525b29659a916f188cd6fb22a12
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-07-13 03:08:55 -0300

    exercicio04

---
+++ js-basico/exercicios-resolvidos/ex04-quero-lucrar.js
@@ -0,0 +1,7 @@
+// Exercício 4 - Quero lucrar
+// Crie um programa que me diga como lucrar nesta situação: quero vender uma bicicleta que 60% do seu valor é R$300, por quanto devo vendê-la para lucrar 25%?
+console.log("\n\n\nExercício 4 - Quero lucrar");
+let valorBike = (300 * 100) / 60;
+let valorBikeComLucro = valorBike * 1.25;
+
+console.log(`Você deve vender a bike por ${valorBikeComLucro} para lucrar 25%`);
